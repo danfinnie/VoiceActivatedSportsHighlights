a64dfb1eb41f54f11990889c0c6e719954c8bfe8
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-12 17:51:40 -0500

    Integrate YouTube player with half time display

--- js/main.js
+++ js/main.js
@@ -31,7 +31,28 @@ $(function() {
 	}
 
 	$('body').on('voice:word', function(ev, word) {
+		
 		if(stops.hasOwnProperty(word))
 			window.sym.play(stops[word]);
 	});
+	
+	$('body').on('voice:word:blackout', function() {
+		$("#myytplayer").css({"top": "10000px"});
+		window.ytplayer.pauseVideo();
+		window.ytplayer.seekTo(120, true);
+		window.ytplayer.pauseVideo();
+	});
+
+	$('body').on('voice:word:action', function() {
+		$("#myytplayer").css({"top": "212px"});
+		window.ytplayer.playVideo();
+	});
 });
+
+function onYouTubePlayerReady(playerId) {
+	window.ytplayer = document.getElementById("myytplayer");
+	window.ytplayer.pauseVideo();
+	window.ytplayer.seekTo(120, true);
+	window.ytplayer.pauseVideo();
+}
+
--- js/youtube.js
+++ js/youtube.js
@@ -1,6 +1,6 @@
 (function() {
 	var params = { allowScriptAccess: "always" };
 	var atts = { id: "myytplayer" };
-	swfobject.embedSWF("https://www.youtube.com/v/Xfv52SEgCNA?enablejsapi=1&playerapiid=ytplayer&version=3",
+	swfobject.embedSWF("https://www.youtube.com/v/VY5a_lRxUn8?enablejsapi=1&playerapiid=ytplayer&version=3",
 		       "ytapiplayer", "433", "250", "8", null, null, params, atts);
 })();
