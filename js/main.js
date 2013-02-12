b525efca971a8a1aeb90b62ca51241e9be9be1bb
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-12 16:13:25 -0500

    Use Google's jQuery lib over Adobe's

--- js/main.js
+++ js/main.js
@@ -1,84 +1,80 @@
 // Examples copied from http://www.adobe.com/devnet/html5/articles/voice-to-drive-the-web-introduction-to-speech-api.html
-window.setTimeout(function() {
-(function($) {
-	$(function() {
-		var $results = $("#results");
-		var $body = $("body");
+$(function() {
+	var $results = $("#results");
+	var $body = $("body");
 
-		$body.on("voice:noise", function() {
-			$results.append(" *");
-		});
+	$body.on("voice:noise", function() {
+		$results.append(" *");
+	});
 
-		$body.on("voice:word", function(ev, word) {
-			$results.append(" " + word);
-		});
+	$body.on("voice:word", function(ev, word) {
+		$results.append(" " + word);
+	});
 
-		$body.on("voice:word:chair", function(ev, word) {
-			$body.css({
-				"background-color": "#"+Math.floor(Math.random()*16777215).toString(16)
-			});
-		});
-
-		$body.on("voice:error", function(ev, err) {
-			$results.html(err);
+	$body.on("voice:word:chair", function(ev, word) {
+		$body.css({
+			"background-color": "#"+Math.floor(Math.random()*16777215).toString(16)
 		});
 	});
 
-	$(function() {
-		var SpeechRecognition = window.SpeechRecognition || 
-					  window.webkitSpeechRecognition || 
-					  window.mozSpeechRecognition || 
-					  window.oSpeechRecognition || 
-					  window.msSpeechRecognition;
+	$body.on("voice:error", function(ev, err) {
+		$results.html(err);
+	});
+});
 
-		var $body = $("body");
-	
-		if (!SpeechRecognition) {
-			$body.trigger("voice:error", ["Please use a browser w/ speech recognition"]);
-			return;
+$(function() {
+	var SpeechRecognition = window.SpeechRecognition || 
+				  window.webkitSpeechRecognition || 
+				  window.mozSpeechRecognition || 
+				  window.oSpeechRecognition || 
+				  window.msSpeechRecognition;
+
+	var $body = $("body");
+
+	if (!SpeechRecognition) {
+		$body.trigger("voice:error", ["Please use a browser w/ speech recognition"]);
+		return;
+	}
+
+	var recognition = new SpeechRecognition();
+	recognition.maxAlternatives = 20;
+	recognition.continuous = true;
+	recognition.interimResults = true;
+
+	recognition.onresult = function(ev) {
+		$.each(ev.results[0], function(i, utterance) {
+			var str = utterance.transcript.trim().toLowerCase();
+			var words = str.split(" ");
+			$.each(words, function(j, word) {
+				$body.trigger("voice:potential-word", [word]);
+			});
+			
+		});
+
+		recognition.stop();
+	};
+
+	// Hash of words to their most recent usages to enable ignoring duplicates that
+	// arise.
+	// TODO: Garbage collect array elements that lasted are before the timeout.
+	var recentWords = {};
+	$body.on("voice:potential-word", function(ev, word) {
+		var time = new Date().getTime() / 1000;
+		var cutoffTime = time - 5;
+		if(!recentWords.hasOwnProperty(word) || recentWords[word] < cutoffTime) {
+			recentWords[word] = time;
+			$body.trigger("voice:word", [word]);
+			$body.trigger("voice:word:"+word);
 		}
-
-		var recognition = new SpeechRecognition();
-		recognition.maxAlternatives = 20;
-		recognition.continuous = true;
-		recognition.interimResults = true;
-
-		recognition.onresult = function(ev) {
-			$.each(ev.results[0], function(i, utterance) {
-				var str = utterance.transcript.trim().toLowerCase();
-				var words = str.split(" ");
-				$.each(words, function(j, word) {
-					$body.trigger("voice:potential-word", [word]);
-				});
-				
-			});
-
-			recognition.stop();
-		};
-
-		// Hash of words to their most recent usages to enable ignoring duplicates that
-		// arise.
-		// TODO: Garbage collect array elements that lasted are before the timeout.
-		var recentWords = {};
-		$body.on("voice:potential-word", function(ev, word) {
-			var time = new Date().getTime() / 1000;
-			var cutoffTime = time - 5;
-			if(!recentWords.hasOwnProperty(word) || recentWords[word] < cutoffTime) {
-				recentWords[word] = time;
-				$body.trigger("voice:word", [word]);
-				$body.trigger("voice:word:"+word);
-			}
-		});
-
-		recognition.onend = function() {
-			recognition.start();
-		};
-
-		recognition.onsoundstart = function() {
-			$body.trigger("voice:noise");
-		};
-
-		recognition.start();
 	});
-})(jQuery);
-}, 5000);
+
+	recognition.onend = function() {
+		recognition.start();
+	};
+
+	recognition.onsoundstart = function() {
+		$body.trigger("voice:noise");
+	};
+
+	recognition.start();
+});
