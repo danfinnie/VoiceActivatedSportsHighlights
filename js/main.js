77d6dc18c280a6b5a667179a7a7aad3a47f490d5
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-12 12:13:15 -0500

    Really basic loading icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+// Examples copied from http://www.adobe.com/devnet/html5/articles/voice-to-drive-the-web-introduction-to-speech-api.html
+(function() {
+	var SpeechRecognition = window.SpeechRecognition || 
+				  window.webkitSpeechRecognition || 
+				  window.mozSpeechRecognition || 
+				  window.oSpeechRecognition || 
+				  window.msSpeechRecognition;
 
+	if (SpeechRecognition) {
+		var recognition = new SpeechRecognition();
+		recognition.maxAlternatives = 5;
+	} else {
+		$("#results").text("Please use a browser w/ speech recognition");
+	}
+})();
