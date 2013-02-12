8274129ae6ecc95b1bc6b8db1228a6628ac86779
Dan Finnie <dfinnie@noisenewyork.com>
2013-02-12 15:55:30 -0500

    Ignore repeated instances of the same word

--- js/main.js
+++ js/main.js
@@ -13,6 +13,12 @@ window.setTimeout(function() {
 			$results.append(" " + word);
 		});
 
+		$body.on("voice:word:chair", function(ev, word) {
+			$body.css({
+				"background-color": "#"+Math.floor(Math.random()*16777215).toString(16)
+			});
+		});
+
 		$body.on("voice:error", function(ev, err) {
 			$results.html(err);
 		});
@@ -26,32 +32,43 @@ window.setTimeout(function() {
 					  window.msSpeechRecognition;
 
 		var $body = $("body");
-
+	
 		if (!SpeechRecognition) {
 			$body.trigger("voice:error", ["Please use a browser w/ speech recognition"]);
 			return;
 		}
 
 		var recognition = new SpeechRecognition();
-		recognition.maxAlternatives = 10;
+		recognition.maxAlternatives = 20;
 		recognition.continuous = true;
 		recognition.interimResults = true;
 
-		// recognition.onaudiostart = function() { $results.text("Starting"); }
-		// recognition.onnomatch = function() { $results.text("Could not recognize"); }
-		// recognition.onerror = function() { $results.append("err"); recognition.start(); }
 		recognition.onresult = function(ev) {
 			$.each(ev.results[0], function(i, utterance) {
-				var str = utterance.transcript.trim();
+				var str = utterance.transcript.trim().toLowerCase();
 				var words = str.split(" ");
 				$.each(words, function(j, word) {
-					$body.trigger("voice:word", [word]);
+					$body.trigger("voice:potential-word", [word]);
 				});
 				
 			});
 
 			recognition.stop();
 		};
+
+		// Hash of words to their most recent usages to enable ignoring duplicates that
+		// arise.
+		// TODO: Garbage collect array elements that lasted are before the timeout.
+		var recentWords = {};
+		$body.on("voice:potential-word", function(ev, word) {
+			var time = new Date().getTime() / 1000;
+			var cutoffTime = time - 5;
+			if(!recentWords.hasOwnProperty(word) || recentWords[word] < cutoffTime) {
+				recentWords[word] = time;
+				$body.trigger("voice:word", [word]);
+				$body.trigger("voice:word:"+word);
+			}
+		});
 
 		recognition.onend = function() {
 			recognition.start();
